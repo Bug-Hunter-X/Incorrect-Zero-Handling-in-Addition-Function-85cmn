094a6e855dac2bdfc7ff6a2aed8a1a92694cb738
BugHunter-X <askzenai@gmail.com>
2025-01-18 17:18:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,5 @@
+function myFunction(a, b) {
+  return a + b; 
+}
+console.log(myFunction(5, 0)); // Expected output: 5, Actual output: 5
+console.log(myFunction(0, 5)); // Expected output: 5, Actual output: 5
